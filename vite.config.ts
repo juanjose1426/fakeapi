9a618d6bbe658e8c8279b68677d43b1b9fb064db
juanjose1426 <juanj.gonzaleza@uniagustiniana.edu.co>
2026-04-27 03:49:40 +0000

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fakeapi/',  // ← reemplaza con el nombre exacto de tu repo
+  base: '/fakeapi/',
 })
